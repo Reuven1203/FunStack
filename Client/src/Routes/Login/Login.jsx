f0febcf1d63d12a5a9e59804a4fd2aa25e82ac72
Adir Ben-David <91300383+beezzyy@users.noreply.github.com>
2023-04-11 22:31:17 -0400

    added tooltip in the login page

--- Client/src/Routes/Login/Login.jsx
+++ Client/src/Routes/Login/Login.jsx
@@ -6,6 +6,10 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -36,6 +40,13 @@ export default function Login() {
   const [firstName, setFirstName] = React.useState('');
   const [pinValue, setPin] = React.useState('');
   const [isSubmitted, setIsSubmitted] = React.useState(false);
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
   const handleNameChange = (event) => {
     setFirstName(event.target.value);
@@ -123,8 +134,20 @@ export default function Login() {
                 required
                 fullWidth
                 name="password"
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
                 label="4-Digit Pin"
-                type="password"
+                type={showPassword ? 'text' : 'password'}
                 id="password"
                 autoComplete="current-password"
                 value={pinValue}
