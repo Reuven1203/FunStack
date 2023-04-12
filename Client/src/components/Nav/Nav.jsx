bf0cac199b29ee09aece60f53a2ea7e266197bc1
Reuven1203 <reuven1203@gmail.com>
2023-04-11 20:20:36 -0400

    nav

--- Client/src/components/Nav/Nav.jsx
+++ Client/src/components/Nav/Nav.jsx
@@ -1,18 +1,57 @@
 import React from 'react';
-import {Box, Drawer, Paper, Typography} from '@mui/material';
+import {Box, Drawer, IconButton, Paper, Typography} from '@mui/material';
 import {Link} from 'react-router-dom';
+import {ArrowBack} from '@mui/icons-material';
+
+const Links = [{
+    name: 'Zoo',
+    icon: '\u{1F6D6}',
+    color: '#ff5555',
+},
+    {
+        name: 'Leaning Lions',
+        icon: '\u{1F981}',
+        color: '#ff9355',
+
+    },
+    {
+        name:'Safari Mates',
+        icon: '\u{1F46B}',
+        secondIcon: '\u{1F43E}',
+        color: '#ffcc55',
+    },
+    {
+        name:'Gorilla Grades',
+        icon: '\u{1F98D}',
+        color: '#55ff55',
+    },
+    {
+        name:'Parrot Chat',
+        icon: '\u{1F99C}',
+        color: '#55ffff',
+    }
+]
 
 const Nav = (props) => {
     return (
             <Drawer hideBackdrop={true}  variant="persistent" open={props.open} onClose={props.onClose}>
-             <Box sx={{display:'flex', flexDirection:'column', width:300, padding:3}}>
-                 <Link>
-                     <Paper sx={{fontSize:50}}>
-                           Link 1
-                     </Paper>
-                 </Link>
-                 <Link>Link</Link>
-                    <Link>Link</Link>
+             <Box sx={{display:'flex', flexDirection:'column', width:'100%', padding:1 ,}}>
+                 <div className="flex flex-col items-center space-y-4 w-full">
+                     {/*back icon to close drawer*/}
+                      <IconButton onClick={props.onClose} size="large">
+                            <ArrowBack fontSize="large"/>
+                        </IconButton>
+                     {Links.map((link, index) => {
+                            return (
+                                <Link key={index} className="w-full">
+                                    <Paper sx={{backgroundColor:`${link.color}`,fontSize:45, width:'100%', display:'flex',alignItems:'center', justifyContent:"center", height:120}}>
+                                        <span className="text-6xl">{link.icon}{link.secondIcon}</span><span className="text-black"> {link.name}</span>
+                                    </Paper>
+                                </Link>
+                            )
+                     })}
+                 </div>
+
              </Box>
             </Drawer>
     );
