e4ecdb3c4d10873985b6e05024ec6c7d2774b6cc
Adir Ben-David <91300383+beezzyy@users.noreply.github.com>
2023-04-16 18:26:58 -0400

    fixed home page and added new reward page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,6 +6,7 @@ import Login from './Routes/Login/Login';
 import SafariMates from './Routes/SafariMates/SafariMates';
 import GorillaGrades from './Routes/GorillaGrades/GorillaGrades';
 import ParrotChat from './Routes/ParrotChat/ParrotChat';
+import JungleRewards from './Routes/JungleRewards/JungleRewards';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="safari-mates" element={<SafariMates />} />
           <Route path="gorilla-grades" element={<GorillaGrades />} />
           <Route path="parrot-chat" element={<ParrotChat />} />
+          <Route path="jungle-rewards" element={<JungleRewards />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- Client/src/Routes/Home/Home.jsx
+++ Client/src/Routes/Home/Home.jsx
@@ -14,26 +14,39 @@ const Home = () => {
     setIsModalOpen(false);
   };
 
+  const storedFirstName = localStorage.getItem('firstName');
+
   return (
     <div className={styles.background}>
       <div className="flex space-x-4 pb-7 h-full justify-center items-center">
-        <div className="flex-col space-y-[50px]">
+        <div className="flex-col space-y-[25px]">
           <Paper
             sx={{
-              backgroundColor: '#38a541',
-              opacity: 0.8,
-              color: 'white',
+              backgroundColor: 'white',
+              color: 'black',
               width: 500,
+              borderRadius: '15px',
+              border: '4px solid',
+              borderColor: '#38a541',
+              textAlign: 'center',
             }}
           >
-            <h1 className="text-white">Hello [Name]</h1>
+            <h1 className="text-green">Hello {storedFirstName}</h1>
             <p>
               Welcome to the FunStack Zoo! Navigate to the top left to continue
               your homework and start learning with the FunStack Zoo!
             </p>
           </Paper>
           <Paper
-            sx={{ backgroundColor: '#dd5860', color: 'white', opacity: 0.8 }}
+            sx={{
+              backgroundColor: 'white',
+              color: 'black',
+              width: 500,
+              borderRadius: '15px',
+              border: '4px solid',
+              borderColor: '#dd5860',
+              textAlign: 'center',
+            }}
           >
             <h1>Next Assignment</h1>
             <h2>Due: March 15, 2025</h2>
@@ -52,12 +65,12 @@ const Home = () => {
           </Card>
         </button>
         <BasicModal
-          title="Contact"
+          title="Teacher Contact Info"
           open={isModalOpen}
           onClose={handleCloseModal}
         >
           <h3 className="text-xl">Email: elnamacias@funstack.com</h3>
-          <h3 className="text-xl">Phone: 555-555-5555</h3>
+          <h3 className="text-xl">Phone: 514-555-8940</h3>
         </BasicModal>
       </div>
     </div>
