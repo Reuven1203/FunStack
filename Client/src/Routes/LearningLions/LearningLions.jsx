497be849776876de4877795c6cd5c4d507ef465d
Adir Ben-David <91300383+beezzyy@users.noreply.github.com>
2023-04-17 02:47:10 -0400

    fixed the LearningLions page

--- Client/src/Routes/LearningLions/LearningLions.jsx
+++ Client/src/Routes/LearningLions/LearningLions.jsx
@@ -2,57 +2,71 @@ import React from 'react';
 import styles from './style.module.css';
 import { Card } from '@mui/material';
 import { Link } from 'react-router-dom';
+import CustomToolTip from '../../components/ToolTip/CustomToolTip';
 const activities = [
   {
     name: 'Shapes',
     link: 'shapes',
     img: 'https://thumbs.dreamstime.com/b/geometric-funny-shapes-cute-kids-colorful-vector-square-cartoon-character-shape-child-education-kindergarten-preschool-212719118.jpg',
     color: 'red',
+    title: 'Click here to play the Shapes game!',
   },
   {
     name: 'Numbers',
     link: 'numbers',
     color: 'blue',
     img: 'https://img.freepik.com/premium-vector/funny-orange-number-five-5-cartoon-character-vector-hand-drawn-illustration_20412-1388.jpg?w=2000',
+    title: 'Click here to play the Numbers game!',
   },
   {
     name: 'Colors',
     link: 'colors',
     color: 'green',
+    title: 'Click here to play the Colors game!',
     img: 'https://previews.123rf.com/images/kharlamova/kharlamova1601/kharlamova160100292/50124734-children-jumping-on-the-background-color-splash-banner-funny-cartoon-character-vector.jpg',
   },
   {
     name: 'Animals',
     link: 'animals',
     color: 'yellow',
+    title: 'Click here to play the Animals game!',
     img: 'https://img.freepik.com/free-vector/cute-animals-white_1308-35096.jpg?w=2000',
   },
 ];
 const LearningLions = () => {
   return (
-    <div className="flex justify-center w-full h-9/10 align-center">
-      <div className="flex flex-wrap space-x-3 space-y-3 h-full justify-center items-center w-[50rem]">
+    <div
+      className="flex flex-col justify-center items-center"
+      style={{ height: '75vh' }}
+    >
+      <h1 className="text-6xl text-center mb-12">
+        Learning Lions
+        {'\u{1F981}'}
+      </h1>
+      <div className="flex justify-center space-x-8">
         {activities.map((activity) => {
           return (
-            <Link to={activity.link}>
-              <Card
-                sx={{
-                  border: '4px solid',
-                  height: 350,
-                  width: 300,
-                  padding: 3,
-                  display: 'flex-col',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  borderRadius: 10,
-                  borderColor: `${activity.color}`,
-                }}
-              >
-                <h1 className="text-center">{activity.name}</h1>
-                <div className="flex w-full h-full">
-                  <img src={activity.img} />
-                </div>
-              </Card>
+            <Link to={activity.link} key={activity.id}>
+              <CustomToolTip title={activity.title}>
+                <Card
+                  sx={{
+                    border: '4px solid',
+                    height: 350,
+                    width: 300,
+                    padding: 3,
+                    display: 'flex-col',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    borderRadius: 10,
+                    borderColor: `${activity.color}`,
+                  }}
+                >
+                  <h2 className="text-center">{activity.name}</h2>
+                  <div className="flex w-full h-full">
+                    <img src={activity.img} alt={activity.name} />
+                  </div>
+                </Card>
+              </CustomToolTip>
             </Link>
           );
         })}
